54ecba4df0260a7b7e2cdfaac3b1250a8d790519
mona mohamed <monaelhoby@yahoo.com>
2020-02-05 02:39:50 +0200

    Updates

---
+++ precache-manifest.f583a6f9c513e1c030f103851c8e2065.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daec0cf7db659c8c8157f3ed0fe31b16",
+    "url": "/Store_app/index.html"
+  },
+  {
+    "revision": "4bd0a28c7d797eaee723",
+    "url": "/Store_app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "90b1ae55d574c9855d76",
+    "url": "/Store_app/static/css/main.31cbf1c6.chunk.css"
+  },
+  {
+    "revision": "4bd0a28c7d797eaee723",
+    "url": "/Store_app/static/js/2.56bc9a4c.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Store_app/static/js/2.56bc9a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b1ae55d574c9855d76",
+    "url": "/Store_app/static/js/main.a91bd062.chunk.js"
+  },
+  {
+    "revision": "bcb5e2836f662af5b13b",
+    "url": "/Store_app/static/js/runtime-main.a8e1ce0e.js"
+  },
+  {
+    "revision": "c7dafc06e0860777d87dcf4c893acd00",
+    "url": "/Store_app/static/media/1.c7dafc06.jpg"
+  },
+  {
+    "revision": "255a33726e1b1370f4936dcdcd7f8413",
+    "url": "/Store_app/static/media/10.255a3372.jpg"
+  },
+  {
+    "revision": "62a28b4810b3f528e6596a194c588a86",
+    "url": "/Store_app/static/media/11.62a28b48.jpg"
+  },
+  {
+    "revision": "b58300fe86d681cd6fd0ff981daafdd4",
+    "url": "/Store_app/static/media/12.b58300fe.jpg"
+  },
+  {
+    "revision": "9571df2d39f724d478ccc36bdc98d729",
+    "url": "/Store_app/static/media/2.9571df2d.jpg"
+  },
+  {
+    "revision": "b1819dcf49afea52445625c0a7c3fba6",
+    "url": "/Store_app/static/media/3.b1819dcf.jpg"
+  },
+  {
+    "revision": "62bfcbc6cb066a538400bc61617e9920",
+    "url": "/Store_app/static/media/4.62bfcbc6.jpg"
+  },
+  {
+    "revision": "03e1679e1184deb86ed7f8abcd042cf6",
+    "url": "/Store_app/static/media/5.03e1679e.jpg"
+  },
+  {
+    "revision": "c4f1b31ba560c6886abfcfc52f691be1",
+    "url": "/Store_app/static/media/6.c4f1b31b.jpg"
+  },
+  {
+    "revision": "a6641e1db5c075ba20c84221c30d702a",
+    "url": "/Store_app/static/media/7.a6641e1d.jpg"
+  },
+  {
+    "revision": "b767ddad14de6753bc257efc3d4be330",
+    "url": "/Store_app/static/media/8.b767ddad.jpg"
+  },
+  {
+    "revision": "1c1fd135e55ecd1cfd00442257db4d7f",
+    "url": "/Store_app/static/media/9.1c1fd135.jpg"
+  },
+  {
+    "revision": "1bc5e3bf60b773b988b232dc8a3d9b7f",
+    "url": "/Store_app/static/media/slider.1bc5e3bf.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Store_app/precache-manifest.f583a6f9c513e1c030f103851c8e2065.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Store_app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
